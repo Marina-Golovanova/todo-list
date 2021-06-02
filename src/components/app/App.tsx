e72a125b519e03784f24e345617f9c2716c830ce
Marina <gma18@list.ru>
2021-06-03 02:06:23 +0300

    feat: add header

---
+++ src/components/app/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.scss";
+import { Header } from "../header/Header";
+
+export const App = () => {
+  return <Header />;
+};
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./header.scss";
+
+export const Header: React.FC = () => {
+  return <header className="header">Todo</header>;
+};
